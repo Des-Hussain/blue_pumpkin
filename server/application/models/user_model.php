b98915843bb180ab882ed35085fb3af063ec19af
maksof-Rehan <31398564+maksof-Rehan@users.noreply.github.com>
2019-04-09 17:37:57 +0500

    solve error

--- server/application/models/user_model.php
+++ server/application/models/user_model.php
@@ -1,22 +1,24 @@
 <?php
 class User_model extends CI_Model{
 	function newUserAdd($usersData){
+		print_r($usersData);
 		$this->db->select('*');
-		$this->db->where('email',$usersData->email);
+		// $this->db->where('email',$usersData->email);
 		$query = $this->db->get('users');
-		if ($query->num_rows() > 0) {
-			return false;
-		}else{
-			$this->db->set('fName', $usersData->fname);
-			$this->db->set('lName', $usersData->lname);
-			$this->db->set('email', $usersData->email);
-			$this->db->set('phone', $usersData->phone);
-			$this->db->set('password', md5($usersData->password));
-			$this->db->set('role', $usersData->role);
-			$this->db->set('active', $usersData->active);
-			$this->db->insert('users');
-			return true;
-		}
+		return $query->result_array();
+		// if ($query->num_rows() > 0) {
+		// 	return false;
+		// }else{
+		// 	$this->db->set('fName', $usersData->fname);
+		// 	$this->db->set('lName', $usersData->lname);
+		// 	$this->db->set('email', $usersData->email);
+		// 	$this->db->set('phone', $usersData->phone);
+		// 	$this->db->set('password', md5($usersData->password));
+		// 	$this->db->set('role', $usersData->role);
+		// 	$this->db->set('active', $usersData->active);
+		// 	$this->db->insert('users');
+		// 	return true;
+		// }
 	}
 }
 ?>
