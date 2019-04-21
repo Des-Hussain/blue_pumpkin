d2a994656150cec2b223e88af8689c3a2d96c25b
Hussain <mohammadhussainmobeen@gmail.com>
2019-04-22 02:28:05 +0500

    landing page complete design

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -10,6 +10,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { SimpleNotificationsModule } from 'angular2-notifications';
 import { MatFormFieldModule} from '@angular/material';
 
+
 // Directives
 import { OnlyNumber } from './directives/number.only.directive';
 import { OnlyNumberNoDecimal } from './directives/no.decimal.directive';
